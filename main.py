d9a97b6985424c216cc1a9659254d51ba00475e8
Cristian Morasso <morassocristian0@gmail.com>
2024-03-05 09:33:54 +0100

    npy save

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import gymnasium as gym
+import numpy as np
 import pandas as pd
 from stable_baselines3 import PPO
 import sys
@@ -24,29 +25,34 @@ config = {
 
 env.unwrapped.configure(config)
 obs, info = env.reset()
-# model = PPO("MlpPolicy", env, verbose=1)
+model = PPO("MlpPolicy", env, verbose=1)
 vec_env = env
 # model.learn(total_timesteps=10)
 # model.save("ppoHigh")
-model = PPO.load("ppoHigh", env=env)
+# model = PPO.load("ppoHigh", env=env)
 obs_act =  []
 vec_env = model.get_env()
 obs = vec_env.reset()
-for i in range(10):
+for i in range(2):
     obs = vec_env.reset()
     done = False
     while not done:
         action, _states = model.predict(obs, deterministic=True)
+        obs_act.append([obs.flatten(), action.flatten()])
         obs, reward, done, info = vec_env.step(action)
         #vec_env.render()
         # VecEnv resets automatically
-        obs_act.append([obs, action])
+        
         # if done:
             
         #     obs = env.reset(seed=i)
 
 env.close()
-oa = pd.DataFrame(obs_act)
-oa.columns = ['obs', 'action']
-oa.to_csv('obs_act.csv')
+
+# oa = pd.DataFrame(obs_act)
+# oa.columns = ['obs', 'action']
+# oa.to_csv('obs_act_test.csv')
+np.save("oa.npy",np.array(obs_act))
+# print(oa["obs"][0])
 sys.stdout.close()
+
